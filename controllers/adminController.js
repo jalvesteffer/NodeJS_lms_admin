c20e28ddd1c2deee4ddfd2e29e0f4394fac45c82
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-05-17 18:59:03 -0400

    Read operations complete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,27 +3,51 @@ var routes = require('express').Router();
 var adminService = require('../services/adminServices');
 
 routes.get('/lms/admin/authors',function(req,res){
-    adminService.getAllAuthors(req,res);
+    adminService.getAllAuthors(req, res);
+});
+
+routes.get('/lms/admin/authors/:id',function(req,res){
+    adminService.getAuthorById(req.params.id, req, res);
 });
 
 routes.get('/lms/admin/books',function(req,res){
     adminService.getAllBooks(req,res);
 });
 
+routes.get('/lms/admin/books/:id',function(req,res){
+    adminService.getBookById(req.params.id, req, res);
+});
+
 routes.get('/lms/admin/publishers',function(req,res){
     adminService.getAllPublishers(req,res);
+});
+
+routes.get('/lms/admin/publishers/:id',function(req,res){
+    adminService.getPublisherById(req.params.id, req, res);
 });
 
 routes.get('/lms/admin/genres',function(req,res){
     adminService.getAllGenres(req,res);
 });
 
+routes.get('/lms/admin/genres/:id',function(req,res){
+    adminService.getGenreById(req.params.id, req, res);
+});
+
 routes.get('/lms/admin/borrowers',function(req,res){
     adminService.getBorrowers(req,res);
 });
 
+routes.get('/lms/admin/borrowers/:id',function(req,res){
+    adminService.getBorrowerById(req.params.id, req, res);
+});
+
 routes.get('/lms/admin/branches',function(req,res){
     adminService.getBranches(req,res);
+});
+
+routes.get('/lms/admin/branches/:id',function(req,res){
+    adminService.getBranchById(req.params.id, req, res);
 });
 
 /* routes.get('/lms/library/branches/branch/:branchId',function(req,res){
--- dao/publisherDao.js
+++ dao/publisherDao.js
@@ -7,3 +7,11 @@ exports.getAllPublishers = function () {
     });
   });;
 };
+
+exports.getPublisherById = function (id) {
+  return new Promise(function (resolve, reject) {
+    db.query('SELECT * FROM library.tbl_publisher WHERE publisherId=?', [id], function (err, result) {
+      return err ? reject(err) : resolve(result);
+    });
+  });;
+};
