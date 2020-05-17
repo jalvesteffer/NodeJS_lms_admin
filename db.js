5d2c3c7ed8f3c862c6be026664a4bbcf009c6aaa
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-05-17 12:01:57 -0400

    Test Git Ignore

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
     user     : 'root',
-    password : 'replace_with_my_password',
+    password : 'replace_with_my_password 2',
     database : 'library'
 });
 
