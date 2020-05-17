2f84983548f957410c78b337eafec3fbc404ee03
jalvesteffer <jason.alvesteffer@smoothstack.com>
2020-05-17 11:57:01 -0400

    package & db file

---
+++ db.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : 'replace_with_my_password',
+    database : 'library'
+});
+
+module.exports = connection;
