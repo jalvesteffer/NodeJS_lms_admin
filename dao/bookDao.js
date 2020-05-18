defc4fafb032f1de46df6a114767ea7ae595b6bc
lexieAA <43833670+lexieAA@users.noreply.github.com>
2020-05-17 22:19:44 -0700

    Here are the MySQL queries updates for the BookDAO

--- dao/bookDao.js
+++ dao/bookDao.js
@@ -2,7 +2,7 @@ var db = require('../db');
 
 exports.getAllBooks = function () {
   return new Promise(function (resolve, reject) {
-    db.query('SELECT * FROM library.tbl_book', function (err, result) {
+    db.query('SELECT ba.bookId, b.title, GROUP_CONCAT(Distinct a.authorName) AS authors, GROUP_CONCAT( Distinct g.genre_name) AS genres FROM library.tbl_author a INNER JOIN library.tbl_book_authors ba ON a.authorId = ba.authorId INNER JOIN library.tbl_book b ON b.bookId = ba.bookId INNER JOIN library.tbl_book_genres bg ON b.bookId = bg.bookId INNER JOIN library.tbl_genre g ON g.genre_id = bg.genre_id GROUP BY bookId ', function (err, result) {
       return err ? reject(err) : resolve(result);
     });
   });;
@@ -10,7 +10,7 @@ exports.getAllBooks = function () {
 
 exports.getBookById = function (id) {
   return new Promise(function (resolve, reject) {
-    db.query('SELECT * FROM library.tbl_book WHERE bookId=?', [id], function (err, result) {
+    db.query('SELECT ba.bookId, b.title, GROUP_CONCAT(Distinct a.authorName) AS authors, GROUP_CONCAT( Distinct g.genre_name) AS genres FROM library.tbl_author a INNER JOIN library.tbl_book_authors ba ON a.authorId = ba.authorId INNER JOIN library.tbl_book b ON b.bookId = ba.bookId INNER JOIN library.tbl_book_genres bg ON b.bookId = bg.bookId INNER JOIN library.tbl_genre g ON g.genre_id = bg.genre_id WHERE b.bookId = ? GROUP BY bookId ', [id], function (err, result) {
       return err ? reject(err) : resolve(result);
     });
   });;
